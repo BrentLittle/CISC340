b5353db952ea1ef2e93e0e155e1b644f98928bf0
david liu <david1liu@hotmail.com>
2019-11-16 12:58:18 -0500

    webservice, serial data updates

--- Python Minicom Replacement/SerialInput.py.py
+++ Python Minicom Replacement/SerialInput.py.py
@@ -1,9 +1,25 @@
 # This is a Functioning Version of our Minicom Python replacement script
+# Added webservice functionality
 
 #!/usr/bin/env python
-import serial
 
+import serial
+from flask import Flask, render_template
+
+app = Flask(__name__)
 ser = serial.Serial("/dev/ttyUSB1", 9600, timeout=2)
 
-while 1:
- print ser.readline()
+def print_web(outstring):
+	return str(outstring)
+
+def main():
+	run_flag = True
+	alarm_flag = False
+	while (run_flag):
+		serial_out = ser.readline()
+		#using the conversion temperature = (analog - 100) / 10
+		current_temp = (int(serial_out) - 100) / 10
+ 
+ if __name__ = "__main__":
+	app.run(deblug=true, host="0.0.0.0", port=6969)
+	main()
