46f5acaaad792c799b6898bdd585fe4200a9eebd
david liu <david1liu@hotmail.com>
2019-11-16 16:48:33 -0500

    added support for 3 basys input values

--- Python Minicom Replacement/serialInput.py
+++ Python Minicom Replacement/serialInput.py
@@ -1,91 +1,131 @@
 #!/usr/bin/env python
+
 import serial
+from flask import Flask, render_template
 import time
 from gpiozero import LED
-ser = serial.Serial("/dev/ttyUSB1", 9600, timeout=2)
-gpioPinsFirstNumber = [LED(x) for x in [5,6,13,19]]
-secondPins = [LED(x) for x in [4,17,27,22]]
-while 1:
-    isCel = ser.readline()
-    isAlarm = ser.readline()
-    temp = int(ser.readline())*0.2 + 8
-    if int(isCel) == 0:
-        temp = (temp-32)//1.8
-    firstDigit = temp //10
-    secondDigit = temp % 10
-    for pin in gpioPinsFirstNumber:
-        pin.on()
-    for pin in secondPins:
-        pin.on()
-    if firstDigit == 9:
-        gpioPinsFirstNumber[1].off()
-        gpioPinsFirstNumber[2].off()
-    elif firstDigit == 8:
-        gpioPinsFirstNumber[1].off()
-        gpioPinsFirstNumber[2].off()
-        gpioPinsFirstNumber[3].off()
-    elif firstDigit == 7:
-        gpioPinsFirstNumber[0].off()
-    elif firstDigit == 6:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[3].off()
-    elif firstDigit == 5:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[2].off()
-    elif firstDigit == 4:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[2].off()
-        gpioPinsFirstNumber[3].off()
-    elif firstDigit == 3:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[1].off()
-    elif firstDigit == 2:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[1].off()
-        gpioPinsFirstNumber[3].off()
-    elif firstDigit == 1:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[1].off()
-        gpioPinsFirstNumber[2].off()
-    else:
-        gpioPinsFirstNumber[0].off()
-        gpioPinsFirstNumber[1].off()
-        gpioPinsFirstNumber[2].off()
-        gpioPinsFirstNumber[3].off()
-    
-    if secondDigit == 9:
-        secondPins[1].off()
-        secondPins[2].off()
-    elif secondDigit == 8:
-        secondPins[1].off()
-        secondPins[2].off()
-        secondPins[3].off()
-    elif secondDigit == 7:
-        secondPins[0].off()
-    elif secondDigit == 6:
-        secondPins[0].off()
-        secondPins[3].off()
-    elif secondDigit == 5:
-        secondPins[0].off()
-        secondPins[2].off()
-    elif secondDigit == 4:
-        secondPins[0].off()
-        secondPins[2].off()
-        secondPins[3].off()
-    elif secondDigit == 3:
-        secondPins[0].off()
-        secondPins[1].off()
-    elif secondDigit == 2:
-        secondPins[0].off()
-        secondPins[1].off()
-        secondPins[3].off()
-    elif secondDigit == 1:
-        secondPins[0].off()
-        secondPins[1].off()
-        secondPins[2].off()
-    else:
-        secondPins[0].off()
-        secondPins[1].off()
-        secondPins[2].off()
-        secondPins[3].off()
-    time.sleep(0.7)
+import threading
+
+app = Flask("appmain")
+temp = 0
+isCel = 0
+isAlarm = 0
+
+@app.route("/")
+def appmain():
+	global temp
+	global isCel
+	global isAlarm
+	if isCel == 0:
+		celFlag = "C"
+	else:
+		celFlag = "F"
+	if isAlarm == 0:
+		alarmFlag = "OFF"
+	else:
+		alarmFlag = "ON"
+	return render_template("index.html", var1 = temp, var2 = celFlag, var3 = alarmFlag)
+
+if __name__ == "__main__" :
+	main()
+	ser = serial.Serial("/dev/ttyUSB1", 9600, timeout=2)
+	thread = threading.Thread(target = serial_start, args = (ser) )
+	thread.start()
+	app.run(debug=True)
+
+def serial_start(ser):
+	gpioPinsFirstNumber = [LED(x) for x in [5,6,13,19]]
+	secondPins = [LED(x) for x in [4,17,27,22]]
+
+	while 1:
+		global temp
+		global isCel
+		global isAlarm
+		
+		isCel = ser.readline()
+		isAlarm = ser.readline()
+		temp = int(ser.readline()) * 0.2 + 8
+		
+		if int(isCel) == 0:
+			temp = (temp - 32) // 1.8
+			
+		firstDigit = temp // 10
+		secondDigit = temp % 10
+		
+		for pin in gpioPinsFirstNumber:
+			pin.on()
+		for pin in secondPins:
+			pin.on()
+		if firstDigit == 9:
+			gpioPinsFirstNumber[1].off()
+			gpioPinsFirstNumber[2].off()
+		elif firstDigit == 8:
+			gpioPinsFirstNumber[1].off()
+			gpioPinsFirstNumber[2].off()
+			gpioPinsFirstNumber[3].off()
+		elif firstDigit == 7:
+			gpioPinsFirstNumber[0].off()
+		elif firstDigit == 6:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[3].off()
+		elif firstDigit == 5:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[2].off()
+		elif firstDigit == 4:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[2].off()
+			gpioPinsFirstNumber[3].off()
+		elif firstDigit == 3:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[1].off()
+		elif firstDigit == 2:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[1].off()
+			gpioPinsFirstNumber[3].off()
+		elif firstDigit == 1:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[1].off()
+			gpioPinsFirstNumber[2].off()
+		else:
+			gpioPinsFirstNumber[0].off()
+			gpioPinsFirstNumber[1].off()
+			gpioPinsFirstNumber[2].off()
+			gpioPinsFirstNumber[3].off()
+		
+		if secondDigit == 9:
+			secondPins[1].off()
+			secondPins[2].off()
+		elif secondDigit == 8:
+			secondPins[1].off()
+			secondPins[2].off()
+			secondPins[3].off()
+		elif secondDigit == 7:
+			secondPins[0].off()
+		elif secondDigit == 6:
+			secondPins[0].off()
+			secondPins[3].off()
+		elif secondDigit == 5:
+			secondPins[0].off()
+			secondPins[2].off()
+		elif secondDigit == 4:
+			secondPins[0].off()
+			secondPins[2].off()
+			secondPins[3].off()
+		elif secondDigit == 3:
+			secondPins[0].off()
+			secondPins[1].off()
+		elif secondDigit == 2:
+			secondPins[0].off()
+			secondPins[1].off()
+			secondPins[3].off()
+		elif secondDigit == 1:
+			secondPins[0].off()
+			secondPins[1].off()
+			secondPins[2].off()
+		else:
+			secondPins[0].off()
+			secondPins[1].off()
+			secondPins[2].off()
+			secondPins[3].off()
+			
+		time.sleep(0.7)
